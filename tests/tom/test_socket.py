21d2db4a3bc119c693bf5c0d05a0ed3fc06a04bf
SEIAROTg <seiarotg@gmail.com>
2019-01-08 14:29:20 +0000

    test(tom): refactor socket test and add ack and retransmission tests

--- tests/tom/test_socket.py
+++ tests/tom/test_socket.py
@@ -1,29 +1,12 @@
 from typing import Tuple, Optional
 import time
 import threading
-from email.mime.multipart import MIMEMultipart
-from unittest.mock import patch, MagicMock, Mock
 import imapclient
-from imapclient.response_types import Envelope, Address
 import pytest
 from faker import Faker
-from src.tom import Endpoint, Credential, Mailbox, Socket
-
-
-def make_envelope(from_: Endpoint, to: Endpoint, sender: Optional[Endpoint] = None) -> Envelope:
-    if sender is None:
-        sender = from_
-    return Envelope(
-        date=None,
-        subject=None,
-        from_=[Address(name=from_.port, route=None, mailbox=from_.address, host=None)],
-        sender=[Address(name=sender.port, route=None, mailbox=sender.address, host=None)],
-        reply_to=None,
-        to=[Address(name=to.port, route=None, mailbox=to.address, host=None)],
-        cc=None,
-        bcc=None,
-        in_reply_to=None,
-        message_id=None)
+from src.tom import Endpoint, Mailbox, Socket
+from .socket_test_helper import SocketTestHelper
+from src.tom.packet import Packet
 
 
 @pytest.fixture()
@@ -31,258 +14,193 @@ def faker() -> Faker:
     return Faker()
 
 
-@pytest.fixture(autouse=True)
-def transport():
-    with patch('smtplib.SMTP') as fixture:
-        yield fixture.return_value
-
-
 @pytest.fixture()
-def store():
-    return MagicMock()
+def helper() -> SocketTestHelper:
+    helper = SocketTestHelper()
+    yield helper
+    print('helper teardown')
+    helper.close()
 
 
-@pytest.fixture()
-def listener():
-    fixture = MagicMock()
-    fixture.idle_check.side_effect = OSError('no idle check in test')
-    return fixture
-
-
-# noinspection PyPep8Naming
-@pytest.fixture(autouse=True)
-def IMAPClient(store, listener):
-    with patch('imapclient.IMAPClient') as fixture:
-        fixture.side_effect = [store, listener]
-        yield fixture
-
-
-# noinspection PyPep8Naming
-@pytest.fixture(autouse=True)
-def Packet():
-    # assume payload is directly fed to mailbox as message
-    with patch('src.tom.mailbox.Packet') as fixture, patch('email.message_from_bytes') as message_from_bytes:
-        message_from_bytes.side_effect = lambda x: x
-        fixture.from_message.side_effect = lambda x: Mock(seq=x[0], payload=x[1])
-        yield fixture
-
-
-@pytest.fixture()
-def credential(faker: Faker) -> Credential:
-    return Credential(host=faker.hostname(), port=faker.pyint(), username=faker.email(), password=faker.password())
-
-
-@pytest.fixture()
-def endpoints(faker: Faker) -> Tuple[Endpoint, Endpoint]:
-    local_endpoint = Endpoint(faker.email(), faker.uuid4())
-    remote_endpoint = Endpoint(faker.email(), faker.uuid4())
-    return local_endpoint, remote_endpoint
-
-
-@pytest.fixture()
-def mailbox(credential: Credential) -> Mailbox:
-    mailbox = Mailbox(credential, credential)
-    yield mailbox
-    mailbox.close()
-
-
-@pytest.fixture()
-def feed_mailbox(store, listener, request):
-    def fixture(messages) -> Tuple[Mailbox, threading.Lock]:
-        ready = threading.Lock()
-
-        def idle_check_stub():
-            ready.acquire()
-            store.fetch.side_effect = [messages]
-            listener.idle_check.side_effect = OSError('quit')
-            return [(len(messages), b'EXISTS')]
-
-        listener.idle_check.side_effect = idle_check_stub
-
-        ready.acquire()
-        mailbox = request.getfixturevalue('mailbox')
-        return mailbox, ready
-
-    return fixture
-
-
-def test_connect(mailbox: Mailbox, endpoints: Tuple[Endpoint, Endpoint]):
-    socket = Socket(mailbox)
-    socket.connect(*endpoints)
+def test_connect(helper: SocketTestHelper):
+    endpoints = helper.fake_endpoints()
+    socket = helper.create_connected_socket(*endpoints)
     socket.close()
 
 
-def test_connect_address_in_use(mailbox: Mailbox, endpoints: Tuple[Endpoint, Endpoint]):
-    socket = Socket(mailbox)
-    socket.connect(*endpoints)
-    socket2 = Socket(mailbox)
+def test_connect_address_in_use(helper: SocketTestHelper):
+    endpoints = helper.fake_endpoints()
+    helper.create_connected_socket(*endpoints)
     with pytest.raises(Exception) as execinfo:
-        socket2.connect(*endpoints)
+        helper.create_connected_socket(*endpoints)
     assert execinfo.match('address already in use')
 
 
-def test_connect_invalid_status(mailbox: Mailbox, endpoints: Tuple[Endpoint, Endpoint]):
-    socket = Socket(mailbox)
-    socket.connect(*endpoints)
+def test_connect_invalid_status(helper: SocketTestHelper):
+    endpoints = helper.fake_endpoints()
+    socket = helper.create_connected_socket(*endpoints)
     with pytest.raises(Exception) as execinfo:
         socket.connect(*reversed(endpoints))
     assert execinfo.match('invalid status of socket')
 
 
-def test_send(transport, Packet, faker: Faker, mailbox: Mailbox, endpoints: Tuple[Endpoint, Endpoint]):
-    msg = MIMEMultipart()
-    Packet.return_value.to_message.return_value = msg
-
+@pytest.mark.timeout(5)
+def test_send(faker: Faker, helper: SocketTestHelper):
+    endpoints = helper.fake_endpoints()
     payload = faker.binary(111)
-    socket = Socket(mailbox)
-    socket.connect(*endpoints)
+    socket = helper.create_connected_socket(*endpoints)
+    packet = Packet(*endpoints, 0, 0, set(), payload)
+
     socket.send(payload)
     socket.close()
 
-    Packet.assert_called_once_with(0, payload)
-    assert msg.get('From') == '"{}" <{}>'.format(endpoints[0].port, endpoints[0].address)
-    assert msg.get('To') == '"{}" <{}>'.format(endpoints[1].port, endpoints[1].address)
-    transport.sendmail.assert_called_once_with(endpoints[0].address, endpoints[1].address, msg.as_bytes())
+    helper.assert_sent(socket, packet)
 
 
 @pytest.mark.timeout(5)
-def test_recv(feed_mailbox, store, faker: Faker, endpoints: Tuple[Endpoint, Endpoint]):
+def test_send_retransmit(faker: Faker, helper: SocketTestHelper):
+    payload = faker.binary(111)
+    endpoints = helper.fake_endpoints()
+    socket = helper.create_connected_socket(*endpoints)
+
+    socket.send(payload)
+    helper.assert_sent(socket, Packet(*endpoints, 0, 0, set(), payload))
+    helper.assert_sent(socket, Packet(*endpoints, 0, 1, set(), payload), 1.5, 0.5)
+
+
+@pytest.mark.timeout(5)
+def test_send_no_retransmit_after_pure_ack(faker: Faker, helper: SocketTestHelper):
+    payload = faker.binary(111)
+    endpoints = helper.fake_endpoints()
+    socket = helper.create_connected_socket(*endpoints)
+
+    socket.send(payload)
+    helper.assert_sent(socket, Packet(*endpoints, 0, 0, set(), payload))
+    helper.feed_messages({faker.pyint(): Packet(*reversed(endpoints), -1, 0, set([(0, 0)]), b'')})
+    helper.assert_not_sent(socket, Packet(*endpoints, 0, 1, set(), payload), 1.5)
+    helper.assert_not_sent(socket, Packet(*endpoints, 0, 1, set([-1]), payload))
+
+
+@pytest.mark.timeout(5)
+def test_recv(faker: Faker, helper: SocketTestHelper):
+    endpoints = helper.fake_endpoints()
     payload = faker.binary(111)
     uid = faker.pyint()
     messages = {
-        uid: {
-            b'ENVELOPE': make_envelope(*reversed(endpoints)),
-            b'BODY[]': (0, payload),
-        }
+        uid: Packet(*reversed(endpoints), 0, 0, set(), payload),
     }
+    socket = helper.create_connected_socket(*endpoints)
 
-    mailbox, ready = feed_mailbox(messages)
-    socket = Socket(mailbox)
-    socket.connect(*endpoints)
-    ready.release()
+    helper.feed_messages(messages)
     ret = socket.recv(len(payload))
-    mailbox.close()
+    socket.close()
 
     assert ret == payload
-    store.add_flags.assert_called_once_with([uid], [imapclient.SEEN])
+    helper.mock_store.add_flags.assert_called_once_with([uid], [imapclient.SEEN])
 
 
 @pytest.mark.timeout(5)
-def test_recv_multiple_packets(feed_mailbox, store, faker: Faker, endpoints: Tuple[Endpoint, Endpoint]):
+def test_recv_multiple_packets(faker: Faker, helper: SocketTestHelper):
+    endpoints = helper.fake_endpoints()
     payloads = [faker.binary(111), faker.binary(36), faker.binary(71)]
     uids = faker.pylist(3, False, int)
     messages = {
-        uids[i]: {
-            b'ENVELOPE': make_envelope(*reversed(endpoints)),
-            b'BODY[]': (i, payloads[i]),
-        } for i in range(3)
+        uids[i]: Packet(*reversed(endpoints), i, 0, set(), payloads[i]) for i in range(3)
     }
+    socket = helper.create_connected_socket(*endpoints)
 
-    mailbox, ready = feed_mailbox(messages)
-    socket = Socket(mailbox)
-    socket.connect(*endpoints)
-    ready.release()
+    helper.feed_messages(messages)
     ret = socket.recv(195)
-    mailbox.close()
+    socket.close()
 
     assert ret == b''.join(payloads)[:195]
-    store.add_flags.assert_called_once_with(uids, [imapclient.SEEN])
+    helper.mock_store.add_flags.assert_called_once_with(uids, [imapclient.SEEN])
 
 
 @pytest.mark.timeout(5)
-def test_recv_multiple_sockets(feed_mailbox, store, faker: Faker, endpoints: Tuple[Endpoint, Endpoint]):
-    endpoints2 = (Endpoint(endpoints[0].address, '!' + endpoints[0].port), endpoints[1])
+def test_recv_multiple_sockets(faker: Faker, helper: SocketTestHelper):
+    endpoints1 = helper.fake_endpoints()
+    endpoints2 = (Endpoint(endpoints1[0].address, '!' + endpoints1[0].port), endpoints1[1])
     payloads = [faker.binary(111), faker.binary(36), faker.binary(1), faker.binary(71), faker.binary(53)]
     uids = faker.pylist(5, False, int)
     seqs = [0, 0, 1, 2, 1]
-    targets = [endpoints, endpoints2, endpoints2, endpoints2, endpoints]
-
+    targets = [endpoints1, endpoints2, endpoints2, endpoints2, endpoints1]
     messages = {
-        uids[i]: {
-            b'ENVELOPE': make_envelope(endpoints[1], targets[i][0]),
-            b'BODY[]': (seqs[i], payloads[i]),
-        } for i in range(5)
+        uids[i]: Packet(*reversed(targets[i]), seqs[i], 0, set(), payloads[i]) for i in range(5)
     }
+    socket1 = helper.create_connected_socket(*endpoints1)
+    socket2 = helper.create_connected_socket(*endpoints2)
 
-    mailbox, ready = feed_mailbox(messages)
-    socket1 = Socket(mailbox)
-    socket1.connect(*endpoints)
-    socket2 = Socket(mailbox)
-    socket2.connect(*endpoints2)
-    ready.release()
+    helper.feed_messages(messages)
     ret1 = socket1.recv(111 + 53)
     ret2 = socket2.recv(36 + 1 + 71)
-    mailbox.close()
+    socket1.close()
+    socket2.close()
 
     assert ret1 == payloads[0] + payloads[4]
     assert ret2 == payloads[1] + payloads[2] + payloads[3]
-    store.add_flags.assert_called_once_with(uids, [imapclient.SEEN])
+    helper.mock_store.add_flags.assert_called_once_with(uids, [imapclient.SEEN])
 
 
 @pytest.mark.timeout(5)
-def test_recv_not_connected(feed_mailbox, store, faker: Faker, endpoints: Tuple[Endpoint, Endpoint]):
+def test_recv_not_connected(faker: Faker, helper: SocketTestHelper):
+    endpoints = helper.fake_endpoints()
     payload = faker.binary(111)
     uid = faker.pyint()
     messages = {
-        uid: {
-            b'ENVELOPE': make_envelope(*reversed(endpoints)),
-            b'BODY[]': (0, payload),
-        }
+        uid: Packet(*reversed(endpoints), 0, 0, set(), payload),
     }
 
-    mailbox, ready = feed_mailbox(messages)
-    ready.release()
-    mailbox.close()
+    helper.feed_messages(messages)
+    helper.close()
 
-    store.add_flags.assert_not_called()
+    helper.mock_store.add_flags.assert_not_called()
 
 
 @pytest.mark.timeout(5)
-def test_recv_invalid_packets(feed_mailbox, store, faker: Faker, Packet, endpoints: Tuple[Endpoint, Endpoint]):
+def test_recv_invalid_packets(faker: Faker, helper: SocketTestHelper):
+    endpoints = helper.fake_endpoints()
     payload = faker.binary(111)
     uid = faker.pyint()
     messages = {
-        uid: {
-            b'ENVELOPE': make_envelope(*reversed(endpoints)),
-            b'BODY[]': (0, payload),
-        }
+        uid: Packet(*reversed(endpoints), 0, 0, set(), payload),
     }
-    Packet.from_message.side_effect = Exception('invalid packet')
+    helper.mock_packet.from_message.side_effect = Exception('invalid packet')
 
-    mailbox, ready = feed_mailbox(messages)
-    ready.release()
-    mailbox.close()
+    helper.feed_messages(messages)
+    helper.close()
 
-    store.add_flags.assert_not_called()
+    helper.mock_store.add_flags.assert_not_called()
 
 
 @pytest.mark.timeout(5)
-def test_recv_order(feed_mailbox, store, faker: Faker, Packet, endpoints: Tuple[Endpoint, Endpoint]):
+def test_recv_order(faker: Faker, helper: SocketTestHelper):
+    endpoints = helper.fake_endpoints()
     payloads = [faker.binary(111), faker.binary(36), faker.binary(71)]
     uids = faker.pylist(3, False, int)
     seqs = [2, 0, 1]
     messages = {
-        uids[i]: {
-            b'ENVELOPE': make_envelope(*reversed(endpoints)),
-            b'BODY[]': (seqs[i], payloads[i]),
-        } for i in range(3)
+        uids[i]: Packet(*reversed(endpoints), seqs[i], 0, set(), payloads[i]) for i in range(3)
     }
+    socket = helper.create_connected_socket(*endpoints)
 
-    mailbox, ready = feed_mailbox(messages)
-    socket = Socket(mailbox)
-    socket.connect(*endpoints)
-    ready.release()
+    helper.feed_messages(messages)
     ret = socket.recv(218)
-    mailbox.close()
+    socket.close()
 
     assert ret == payloads[1] + payloads[2] + payloads[0]
-    store.add_flags.assert_called_once_with(uids, [imapclient.SEEN])
+    helper.mock_store.add_flags.assert_called_once_with(uids, [imapclient.SEEN])
 
 
 @pytest.mark.timeout(5)
-def test_close_unblock_recv(mailbox: Mailbox, endpoints: Tuple[Endpoint, Endpoint]):
-    socket = Socket(mailbox)
-    socket.connect(*endpoints)
+def test_recv_timeout(helper: SocketTestHelper):
+    socket = helper.create_connected_socket()
+    socket.recv(100, timeout=0)
+
+
+@pytest.mark.timeout(5)
+def test_close_unblock_recv(helper: SocketTestHelper):
+    socket = helper.create_connected_socket()
 
     def close():
         time.sleep(0.2)
@@ -291,3 +209,37 @@ def test_close_unblock_recv(mailbox: Mailbox, endpoints: Tuple[Endpoint, Endpoin
     thread.start()
     socket.recv(100)
     thread.join()
+
+
+@pytest.mark.timeout(5)
+def test_recv_ack(faker: Faker, helper: SocketTestHelper):
+    payload = faker.binary(111)
+    endpoints = helper.fake_endpoints()
+    socket = helper.create_connected_socket(*endpoints)
+
+    helper.feed_messages({faker.pyint(): Packet(*reversed(endpoints), 0, 0, set(), payload)})
+    helper.assert_sent(socket, Packet(*endpoints, -1, 0, set([(0, 0)]), b''), 1.5, 0.5)
+
+
+@pytest.mark.timeout(5)
+def test_recv_ack_to_non_pure_ack(faker: Faker, helper: SocketTestHelper):
+    payload = faker.binary(111)
+    endpoints = helper.fake_endpoints()
+    socket = helper.create_connected_socket(*endpoints)
+
+    socket.send(payload)
+    helper.assert_sent(socket, Packet(*endpoints, 0, 0, set(), payload))
+    helper.feed_messages({faker.pyint(): Packet(*reversed(endpoints), 0, 0, set([(0, 0)]), payload)})
+    helper.assert_sent(socket, Packet(*endpoints, -1, 0, set([(0, 0)]), b''))
+
+
+@pytest.mark.timeout(5)
+def test_recv_no_ack_to_pure_ack(faker: Faker, helper: SocketTestHelper):
+    payload = faker.binary(111)
+    endpoints = helper.fake_endpoints()
+    socket = helper.create_connected_socket(*endpoints)
+
+    socket.send(payload)
+    helper.assert_sent(socket, Packet(*endpoints, 0, 0, set(), payload))
+    helper.feed_messages({faker.pyint(): Packet(*reversed(endpoints), -1, 0, set([(0, 0)]), b'')})
+    helper.assert_not_sent(socket, Packet(*endpoints, -1, 0, set([(0, 0)]), b''), 1.5)
