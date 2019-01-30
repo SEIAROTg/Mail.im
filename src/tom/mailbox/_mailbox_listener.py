ae56026e2db37e57058195ebc09d1c75843022f9
SEIAROTg <seiarotg@gmail.com>
2019-01-30 16:46:04 +0000

    refactor(tom): discard envelope and use info from email body and packets

--- src/tom/mailbox/_mailbox_listener.py
+++ src/tom/mailbox/_mailbox_listener.py
@@ -56,7 +56,7 @@ class MailboxListener(MailboxTasks):
 
     def __check_new_packets(self):
         uids = self.__store.search('UNSEEN')
-        messages = self.__store.fetch(uids, ['ENVELOPE', 'BODY.PEEK[]'])
+        messages = self.__store.fetch(uids, ['BODY.PEEK[]'])
         seens = []
         for uid, message in messages.items():
             try:
@@ -70,56 +70,49 @@ class MailboxListener(MailboxTasks):
 
     def __process_incoming_packet(self, message: Dict[bytes, Any]) -> bool:
         msg = email.message_from_bytes(message[b'BODY[]'])
-        envelope: imapclient.response_types.Envelope = message[b'ENVELOPE']
-        if envelope.sender[0].mailbox != envelope.from_[0].mailbox:
-            raise Exception('invalid packet: inconsistent sender and from header')
         packet = Packet.from_message(msg)
-        remote_endpoint = Endpoint(envelope.from_[0].mailbox, envelope.from_[0].name)
 
-        seen: bool = False
         with self._mutex:
-            for to in envelope.to:
-                local_endpoint = Endpoint(to.mailbox, to.name)
-                sid = self._connected_sockets.get((local_endpoint, remote_endpoint))
-                if sid is not None:  # connected socket
-                    context: _socket_context.Connected = self._sockets[sid]
-                    with context.cv:
-                        for ack_seq, ack_attempt in packet.acks:
-                            self.__process_ack(sid, ack_seq, ack_attempt)
-                        if packet.seq != -1:  # no action for pure ack
-                            context.pending_remote[packet.seq] = packet.payload
-                            context.to_ack.add((packet.seq, packet.attempt))
-                            self._schedule_task(
-                                src.config.config['tom']['ATO'] / 1000,
-                                functools.partial(self._task_send_ack, sid, context.next_seq))
-                            if packet.payload and packet.seq == context.recv_cursor[0]:
-                                self._socket_update_ready_status(sid, 'read', True)
-                            context.cv.notify_all()
-                    seen = True
-                    continue
-
-                sid = next((sid
-                            for sid, listening_endpoint in self._listening_sockets.items()
-                            if listening_endpoint.matches(local_endpoint)), None)
-                if sid is not None:  # listening socket
-                    context: _socket_context.Listening = self._sockets[sid]
-                    with context.cv:
-                        conn_sid = context.connected_sockets.get((local_endpoint, remote_endpoint))
-                        if conn_sid: # existing pending connection
-                            conn_context = context.sockets[sid]
-                        else:  # new pending connection
-                            conn_sid = self._socket_allocate_id()
-                            conn_context = _socket_context.Connected(local_endpoint, remote_endpoint)
-                            context.sockets[conn_sid] = conn_context
-                            context.connected_sockets[(local_endpoint, remote_endpoint)] = conn_sid
-                            context.queue.append(conn_sid)
+            sid = self._connected_sockets.get((packet.to, packet.from_))
+            if sid is not None:  # connected socket
+                context: _socket_context.Connected = self._sockets[sid]
+                with context.cv:
+                    for ack_seq, ack_attempt in packet.acks:
+                        self.__process_ack(sid, ack_seq, ack_attempt)
+                    if packet.seq != -1 and packet.seq >= context.recv_cursor[0]:
+                        # no action for pure ack and duplicated packets
+                        context.pending_remote[packet.seq] = packet.payload
+                        context.to_ack.add((packet.seq, packet.attempt))
+                        self._schedule_task(
+                            src.config.config['tom']['ATO'] / 1000,
+                            functools.partial(self._task_send_ack, context, context.next_seq))
+                        if packet.payload and packet.seq == context.recv_cursor[0]:
                             self._socket_update_ready_status(sid, 'read', True)
-                            context.cv.notify_all()
-                        conn_context.pending_remote[packet.seq] = packet.payload
-                        conn_context.to_ack.add((packet.seq, packet.attempt))
-                    seen = True
-                    continue
-        return seen
+                        context.cv.notify_all()
+                return True
+
+            sid = next((sid
+                        for sid, listening_endpoint in self._listening_sockets.items()
+                        if listening_endpoint.matches(packet.to)), None)
+            if sid is not None:  # listening socket
+                context: _socket_context.Listening = self._sockets[sid]
+                with context.cv:
+                    conn_sid = context.connected_sockets.get((packet.to, packet.from_))
+                    if conn_sid: # existing pending connection
+                        conn_context = context.sockets[sid]
+                    else:  # new pending connection
+                        conn_sid = self._socket_allocate_id()
+                        conn_context = _socket_context.Connected(packet.to, packet.from_)
+                        context.sockets[conn_sid] = conn_context
+                        context.connected_sockets[(packet.to, packet.from_)] = conn_sid
+                        context.queue.append(conn_sid)
+                        self._socket_update_ready_status(sid, 'read', True)
+                        context.cv.notify_all()
+                    conn_context.pending_remote[packet.seq] = packet.payload
+                    conn_context.to_ack.add((packet.seq, packet.attempt))
+                return True
+
+        return False
 
     def __process_ack(self, sid: int, seq: int, attempt: int):
         context: _socket_context.Connected = self._socket_check_status(sid, _socket_context.Connected)
