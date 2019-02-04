4374da994c5d17e97ffef62f018f11c13835b9da
SEIAROTg <seiarotg@gmail.com>
2019-02-04 19:59:16 +0000

    refactor(tom): (de)serialize packets with protobuf

--- src/tom/mailbox/_packet.py
+++ src/tom/mailbox/_packet.py
@@ -3,10 +3,10 @@ from typing import Set, Tuple
 import email.message
 import email.encoders
 from email.utils import parseaddr, formataddr
-from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 from dataclasses import dataclass
 from .. import Endpoint
+from . import packet_pb2
 import src.config
 
 
@@ -25,37 +25,29 @@ class Packet:
             raise Exception('invalid packet: invalid X-Mailer header')
         from_ = Endpoint(*reversed(parseaddr(msg.get('From'))))
         to = Endpoint(*reversed(parseaddr(msg.get('To'))))
-        try:
-            seq, attempt = msg.get('Subject').rsplit('-', 1)
-            seq = int(seq)
-            attempt = int(attempt)
-        except ValueError or AttributeError:
-            raise Exception('invalid packet: invalid tid')
-        try:
-            acks, payload = msg.get_payload()
-            acks = acks.get_payload()
-            if acks != '':
-                acks = acks.split('|')
-                for i in range(len(acks)):
-                    ack_seq, ack_attempt = acks[i].split('-')
-                    acks[i] = (int(ack_seq), int(ack_attempt))
-                acks = set(acks)
-            else:
-                acks = set()
-        except ValueError:
-            raise Exception('invalid packet: invalid acks')
-        payload = payload.get_payload(decode=True)
+
+        packet = packet_pb2.Packet()
+        packet.ParseFromString(msg.get_payload(decode=True))
+        seq = packet.id.seq
+        attempt = packet.id.attempt
+        acks = set((id.seq, id.attempt) for id in packet.acks)
+        payload = packet.payload
         return Packet(from_, to, seq, attempt, acks, payload)
 
     def to_message(self: Packet) -> email.message.Message:
-        body = MIMEMultipart()
-        serialized_acks = '|'.join('-'.join(map(str, ack)) for ack in self.acks)
-        acks = MIMEApplication(serialized_acks, 'x-mailim-acks', email.encoders.encode_noop)
-        payload = MIMEApplication(self.payload, 'x-mailim-payload', email.encoders.encode_base64)
-        body.attach(acks)
-        body.attach(payload)
+        packet = packet_pb2.Packet()
+        packet.id.seq = self.seq
+        packet.id.attempt = self.attempt
+        acks = []
+        for seq, attempt in self.acks:
+            id = packet_pb2.PacketId()
+            id.seq = seq
+            id.attempt = attempt
+            acks.append(id)
+        packet.acks.extend(acks)
+        packet.payload = self.payload
+        body = MIMEApplication(packet.SerializeToString(), 'x-mailim-packet', email.encoders.encode_base64)
         body.add_header('X-Mailer', src.config.config['tom']['X-Mailer'])
-        body.add_header('Subject', '-'.join(map(str, (self.seq, self.attempt))))
         body.add_header('From', formataddr((self.from_.port, self.from_.address)))
         body.add_header('To', formataddr((self.to.port, self.to.address)))
         return body
--- tests/tom/test_packet.py
+++ tests/tom/test_packet.py
@@ -6,65 +6,23 @@ from src.tom.mailbox._packet import Packet
 from src.config import config
 
 
-def make_msg_txt(
-        from_=b'port A <foo@bar.com>',
-        to=b'port B <bar@foo.com>',
-        seq=b'123',
-        attempt=b'456',
-        xmailer=bytes(config['tom']['X-Mailer'], 'ascii'),
-        acks=b'123-0|456-999|789-1|1111-234',
-        payload=b'asdfghjklqwertyuiopzxcvbnm'):
-    return b'Content-Type: multipart/mixed; boundary="===============5805447368894275178=="\r\nFrom: %b\r\nTo: %b\r\nSubject: %b-%b\r\nX-Mailer: %b\r\n\r\n--===============5805447368894275178==\nContent-Type: application/mailim-acks\nMIME-Version: 1.0\nContent-Transfer-Encoding: quoted-printable\n\n%b\n--===============5805447368894275178==\r\nnContent-Type: application/mailim-payload\r\nContent-Transfer-Encoding: 8bit\r\n\r\n%b\r\n--===============5805447368894275178==--\r\n'\
-        % (from_, to, seq, attempt, xmailer, acks, payload)
-
-
-def test_from_message(faker: Faker):
+@pytest.fixture()
+def packet(faker: Faker) -> Packet:
     from_ = Endpoint(faker.email(), faker.uuid4())
     to = Endpoint(faker.email(), faker.uuid4())
     seq = faker.pyint()
     attempt = faker.pyint()
-    acks = faker.pylist(10, False, int)
-    acks = set(zip(acks[0::2], acks[1::2]))
+    acks = set((faker.pyint(), faker.pyint()) for i in range(10))
     payload = faker.binary(111)
-    msg_txt = make_msg_txt(
-        from_=bytes('{} <{}>'.format(from_.port, from_.address), 'ascii'),
-        to=bytes('{} <{}>'.format(to.port, to.address), 'ascii'),
-        seq=b'%d' % seq,
-        attempt=b'%d' % attempt,
-        acks=b'|'.join(b'%d-%d' % ack for ack in acks),
-        payload=payload)
-    msg = email.message_from_bytes(msg_txt)
-    packet = Packet.from_message(msg)
-    assert packet.from_ == from_
-    assert packet.to == to
-    assert packet.seq == seq
-    assert packet.attempt == attempt
-    assert packet.acks == acks
-    assert packet.payload == payload
+    return Packet(from_, to, seq, attempt, acks, payload)
 
 
-def test_from_message_invalid_mailer(faker: Faker):
-    msg_txt = make_msg_txt(xmailer=b'!'+bytes(config['tom']['X-Mailer'], 'ascii'))
-    msg = email.message_from_bytes(msg_txt)
+def test_from_message_invalid_mailer(packet: Packet):
+    msg = packet.to_message()
+    msg.replace_header('X-Mailer', b'!' + bytes(config['tom']['X-Mailer'], 'ascii'))
     with pytest.raises(Exception) as execinfo:
         Packet.from_message(msg)
     assert execinfo.match('invalid X-Mailer header')
-
-
-def test_from_message_invalid_tid(faker: Faker):
-    msg_txt = make_msg_txt(seq=b'!')
-    msg = email.message_from_bytes(msg_txt)
-    with pytest.raises(Exception) as execinfo:
-        Packet.from_message(msg)
-    assert execinfo.match('invalid tid')
-
-
-def test_from_message_invalid_acks(faker: Faker):
-    msg_txt = make_msg_txt(acks=b'!')
-    msg = email.message_from_bytes(msg_txt)
-    with pytest.raises(Exception) as execinfo:
-        Packet.from_message(msg)
-    assert execinfo.match('invalid acks')
 
 
 def test_to_message(faker: Faker):
@@ -76,15 +34,16 @@ def test_to_message(faker: Faker):
     payload = faker.binary(111)
     packet = Packet(from_, to, seq, attempt, acks, payload)
     msg = packet.to_message()
+    assert msg.get('Content-Type') == 'application/x-mailim-packet'
     assert msg.get('From') == '{} <{}>'.format(from_.port, from_.address)
     assert msg.get('To') == '{} <{}>'.format(to.port, to.address)
-    assert msg.get('Subject') == '-'.join(map(str, (seq, attempt)))
     assert msg.get('X-Mailer') == config['tom']['X-Mailer']
-    assert len(msg.get_payload()) == 2
-    assert msg.get_payload()[0].get('Content-Type') == 'application/x-mailim-acks'
-    assert msg.get_payload()[0].get_payload() == '|'.join('-'.join(map(str, ack)) for ack in acks)
-    assert msg.get_payload()[1].get('Content-Type') == 'application/x-mailim-payload'
-    assert msg.get_payload()[1].get_payload(decode=True) == payload
+
+
+def test_from_to_message(packet: Packet):
+    msg = packet.to_message()
+    packet_recv = Packet.from_message(msg)
+    assert packet_recv == packet
 
 
 def test_negative_seq(faker: Faker):
